24609a06ae7c382ab26eae87f0c3840e1aaf0c2b
rgilangn <ghz.527.re@gmail.com>
2020-06-24 20:46:44 +0700

    revision SK2_0

--- MixautomatedEx.py
+++ MixautomatedEx.py
@@ -22,8 +22,8 @@ def mainTopo():
     Se2 = net.addHost('Se2', ip='192.168.2.2/29')
     Ro1 = net.addHost('Router1')
 
-    net.addLink(Cl1, Ro1, bw=1)
-    net.addLink(Se2, Ro1, bw=100) #, max_queue_size = maxQ
+    net.addLink(Cl1, Ro1, bw=10)
+    net.addLink(Se2, Ro1, bw=1) #, max_queue_size = maxQ
 
 
     net.build()
@@ -60,7 +60,7 @@ def mainTopo():
     print('===========================================================================')
 
     Se2.cmd('iperf -s &')
-    # Se2.cmd('iperf -s  > dataResult/examine3/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_iperf-server.txt &')
+    # Se2.cmd('iperf -s  > dataResult/examine-X/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_iperf-server.txt &')
     print('                          Server Iperf Started')
     Se2.cmd('python -m SimpleHTTPServer &')
     print('                          Python HTTP Server Start')
@@ -69,32 +69,34 @@ def mainTopo():
     print('                TCPDUMP Started Longlived for 65 s Please Wait')
     print('                              Iperf Started')
     Cl1.cmd('tcpdump -G 60 -W 1 -w /home/reghn/Documents/pcapngs/_LL_.pcapng -i Cl1-eth0 &') #62s
-    Cl1.cmd('iperf -c 192.168.2.2 -t 60 -i 1 > dataResult/examine3/_LL_iperfRests.txt &') #60s
-    Cl1.cmd('ping 192.168.2.2 -c 61 > dataResult/examine3/_LL_rttRests.txt &') #61s
+    Cl1.cmd('iperf -c 192.168.2.2 -t 60 -i 1 > dataResult/examine-X/_LL_iperfRests.txt &') #60s
+    Cl1.cmd('ping 192.168.2.2 -c 61 > dataResult/examine-X/_LL_rttRests.txt &') #61s
     
-    time.sleep(10)
+    time.sleep(5)
+
+
     os.system('echo                TCPDUMP Shortlived Started for 10 s Please Wait')
     Cl1.cmdPrint('wget 192.168.2.2:8000')
     os.system('scrot --delay 2 restSL.png')
 
 
-    # os.system('mv /home/reghn/Documents/pcapngs/_LL_.pcapng /home/reghn/Documents/pcapngs/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_.pcapng')
-    # os.system('mv dataResult/examine3/_LL_iperfRests.txt dataResult/examine3/'+str(ccName)+'_'+str(maxQ)+'mix_LL_iperfRests.txt')
-    # os.system('mv dataResult/examine3/_LL_rttRests.txt dataResult/examine3/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_rttRests.txt')
+    os.system('mv /home/reghn/Documents/pcapngs/_LL_.pcapng /home/reghn/Documents/pcapngs/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_.pcapng')
+    os.system('mv dataResult/examine-X/_LL_iperfRests.txt dataResult/examine-X/'+str(ccName)+'_'+str(maxQ)+'mix_LL_iperfRests.txt')
+    os.system('mv dataResult/examine-X/_LL_rttRests.txt dataResult/examine-X/'+str(ccName)+'_'+str(maxQ)+'_mix_LL_rttRests.txt')
     print('=========================================================================')
     
     
     
     print('=========================================================================')
-    #Cl1.cmd('tcpdump -G 10 -W 1 -w /home/reghn/Documents/pcapngs/_SL_.pcapng -i Cl1-eth0 &')
+    # Cl1.cmd('tcpdump -G 10 -W 1 -w /home/reghn/Documents/pcapngs/_SL_.pcapng -i Cl1-eth0 &')
     
     print("                         Processing all file's   ")
     
     #os.system('mv /home/reghn/Documents/pcapngs/_SL_.pcapng /home/reghn/Documents/pcapngs/'+str(ccName)+'_'+str(maxQ)+'_SL_.pcapng')
-    # os.system('mv restSL.png restSL'+str(ccName)+'_'+str(maxQ)+'_SK1')
+    os.system('mv restSL.png restSL'+str(ccName)+'_'+str(maxQ)+'_SK2')
     print('=========================================================================')
     time.sleep(60)
-    #CLI(net)
+    CLI(net)
     net.stop()
 
 def runAll():
@@ -108,7 +110,8 @@ def runAll():
 if __name__ =='__main__':
     setLogLevel('info')
     
-    maxq = [20, 200, 2000, 20000]
+    maxq = [2000, 20000]
     for maxQ in maxq:
         runAll()
+    # runAll()
     
